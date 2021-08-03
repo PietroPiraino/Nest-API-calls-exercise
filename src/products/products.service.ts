eb29906b15657273914dcfdebf514db8b2070228
Pietro Piraino <Kondom91@Pietros-MacBook-Pro.local>
2021-08-03 11:33:05 +0200

    first-commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from './product.model';
+
+@Injectable()
+export class ProductsService {
+  products: Product[] = [];
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex((prod) => prod.id === id);
+    const product = this.products[productIndex];
+    if (!product) {
+      throw new NotFoundException('Prodotto non presente');
+    }
+    return [product, productIndex];
+  }
+
+  insertProduct(title: string, description: string, price: number) {
+    const prodId = Math.random().toString();
+    const newProduct = new Product(prodId, title, description, price);
+    this.products.push(newProduct);
+    return prodId;
+  }
+
+  getProducts() {
+    return [...this.products];
+  }
+
+  getProductById(productId: string) {
+    const product = this.findProduct(productId)[0];
+    return { ...product };
+  }
+
+  updateProduct(
+    productId: string,
+    title: string,
+    description: string,
+    price: number,
+  ) {
+    const [product, index] = this.findProduct(productId);
+    const updatedProduct = { ...product };
+
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (description) {
+      updatedProduct.description = description;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    this.products[index] = updatedProduct;
+  }
+
+  deleteProduct(prodId: string) {
+    const index = this.findProduct(prodId)[1];
+    this.products.splice(index, 1);
+  }
+}
